ae9e04a9581c54890ff24ff684cbaed7209e3a1e
w4560000 <39783609+w4560000@users.noreply.github.com>
2020-12-23 14:11:24 +0800

    add NamedPipeServerStream

---
+++ NamedPipeServerStream_Client/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace NamedPipeServerStream_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            while(true)
+            {
+                using (var clientStream = new System.IO.Pipes.NamedPipeClientStream("127.0.0.1", "NamedPipeServerStream_Test"))
+                {
+                    Console.WriteLine("before Connect()");
+                    clientStream.Connect(1000);
+                    clientStream.ReadMode = System.IO.Pipes.PipeTransmissionMode.Message;
+
+                    do
+                    {
+                        byte[] bytes = new byte[4];
+                        clientStream.Read(bytes, 0, 4);
+                        int val = BitConverter.ToInt32(bytes, 0);
+                        Console.WriteLine("NewID == " + val + "\r");
+                    } while (!clientStream.IsMessageComplete);
+                }
+
+                Thread.Sleep(1);
+            };
+        }
+    }
+}
---
+++ NamedPipeServerStream_Server/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO.Pipes;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NamedPipeServerStream_Server
+{
+    class Program
+    {
+        private static volatile int _newId = 0;
+
+        /// <summary>
+        /// NamedPipeServerStream Server端
+        /// 
+        /// 建立管線
+        /// 
+        /// Client 透過 PipeName 來連線
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Task.Run(() =>
+            {
+                NamedPipeServerStream stream = null;
+                Console.WriteLine("start server in thread " + Thread.CurrentThread.ManagedThreadId);
+
+                // maxNumberOfServerInstances = 允許建立相同PipeName的個數
+                stream = new NamedPipeServerStream("NamedPipeServerStream_Test",
+                     PipeDirection.InOut,
+                     1,
+                     PipeTransmissionMode.Message,
+                     PipeOptions.None);
+
+                while (true)
+                {
+                    Console.WriteLine("before WaitForConnection()");
+                    stream.WaitForConnection();
+                    Thread.Sleep(1000);
+
+                    int newId = ++_newId;
+
+                    byte[] bytes = BitConverter.GetBytes(newId);
+                    stream.Write(bytes, 0, bytes.Length);
+                    stream.Flush();
+                    Console.WriteLine($"[{DateTime.Now:yyyyMMdd hhmmss}]Send newId: {newId}.");
+                    stream.Disconnect();
+                }
+            });
+
+            Console.Read();
+        }
+    }
+}
