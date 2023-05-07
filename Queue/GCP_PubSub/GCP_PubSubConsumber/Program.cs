59d932ea156aad8d9d7c225ec8e0e7945afc7b43
w4560000 <39783609+w4560000@users.noreply.github.com>
2023-05-08 00:49:36 +0800

    新增 GCP PubSub 範例

--- Queue/GCP_PubSub/GCP_PubSubConsumber/Program.cs
+++ Queue/GCP_PubSub/GCP_PubSubConsumber/Program.cs
@@ -1,33 +1,64 @@
 ﻿using Google.Cloud.PubSub.V1;
-using System.Text.Json;
+using Grpc.Core;
 
 namespace GCP_PubSubConsumber
 {
     internal class Program
     {
-        static async Task Main(string[] args)
+        private static async Task Main(string[] args)
         {
             Console.WriteLine(" Start GCP_PubSubConsumber");
 
             // 憑證
-            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", @"D:\Lab\PubSub\pubsub-credential.json");
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", @"C:\Lab\PubSub\pubsub-credential.json");
 
-            SubscriptionName subscriptionName = new SubscriptionName("perfect-eon-383414", "Test-Topic-Sub01");
+            await StreamPull();
+
+            //await Pull();
+            Console.ReadKey();
+        }
+
+        private static async Task StreamPull()
+        {
+            SubscriptionName subscriptionName = new SubscriptionName("project-id", "Test-Topic-Sub02");
             SubscriberClient subscriber = await SubscriberClient.CreateAsync(subscriptionName);
 
-            await subscriber.StartAsync((msg, cancellationToken) =>
+            await subscriber.StartAsync(async (msg, cancellationToken) =>
             {
-                string now = DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss.fff");
-                string guid = Guid.NewGuid().ToString();
-                Console.WriteLine($"{guid} Received message {msg.MessageId} published at {msg.PublishTime.ToDateTime()}");
-                Console.WriteLine($"{guid} Text: '{msg.Data.ToStringUtf8()}'");
-                Console.WriteLine("\n");
-
+                Console.WriteLine($"MessageId {msg.MessageId}, Message: {msg.Data.ToStringUtf8()}, PublishTime {msg.PublishTime.ToDateTime()}");
+                //Console.WriteLine($"MessageId {msg.MessageId}, Delay");
+                //await Task.Delay(20000);
+                //Console.WriteLine($"MessageId {msg.MessageId}, Delay End");
                 // Return Reply.Ack to indicate this message has been handled.
-                return Task.FromResult(SubscriberClient.Reply.Ack);
+                return await Task.FromResult(SubscriberClient.Reply.Ack);
             });
+        }
 
-            Console.ReadKey();
+        private static async Task Pull()
+        {
+            SubscriptionName subscriptionName = new SubscriptionName("project-id", "Test-Topic-Sub01");
+            SubscriberServiceApiClient subscriberClient = SubscriberServiceApiClient.Create();
+            int messageCount = 0;
+
+            try
+            {
+                Console.WriteLine($"Start Pull, {DateTime.Now:yyyyMMdd HH:mm:ss.fff}");
+                PullResponse response = await subscriberClient.PullAsync(subscriptionName, maxMessages: 10);
+                Console.WriteLine($"End Pull, {DateTime.Now:yyyyMMdd HH:mm:ss.fff}");
+
+                foreach (ReceivedMessage msg in response.ReceivedMessages)
+                {
+                    string text = System.Text.Encoding.UTF8.GetString(msg.Message.Data.ToArray());
+                    Console.WriteLine($"MessageId: {msg.Message.MessageId}, Message: {text}, PublishTime: {msg.Message.PublishTime}");
+                    Interlocked.Increment(ref messageCount);
+
+                    subscriberClient.Acknowledge(subscriptionName, response.ReceivedMessages.Select(msg => msg.AckId));
+                }
+            }
+            catch (RpcException ex) when (ex.Status.StatusCode == StatusCode.Unavailable)
+            {
+                // UNAVAILABLE due to too many concurrent pull requests pending for the given subscription.
+            }
         }
     }
 }
--- Queue/GCP_PubSub/GCP_PubSubProducer/Program.cs
+++ Queue/GCP_PubSub/GCP_PubSubProducer/Program.cs
@@ -9,19 +9,27 @@ namespace GCP_PubSubProducer
             Console.WriteLine(" Start GCP_PubSubProducer");
 
             // 憑證
-            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", @"D:\Lab\PubSub\pubsub-credential.json");
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", @"C:\Lab\PubSub\pubsub-credential.json");
 
-            TopicName topicName = new TopicName("perfect-eon-383414", "Test-Topic");
+            TopicName topicName = new TopicName("project-id", "Test-Topic");
             PublisherClient publisher = await PublisherClient.CreateAsync(topicName);
 
-            await Task.WhenAll(
-                 Enumerable.Range(1, 10).Select(async item =>
-                 {
-                     string msg = $"{item}";
-                     string messageId = await publisher.PublishAsync(msg);
-                     Console.WriteLine($"{DateTime.Now:yyyy/MM/dd HH:mm:ss.fff} messageId:{messageId}, Sent {msg}");
-                 })
-                );
+            Enumerable.Range(1, 10).ToList().ForEach(item =>
+            {
+                string msg = $"{item}";
+                string messageId = publisher.PublishAsync(msg).GetAwaiter().GetResult();
+                Console.WriteLine($"{DateTime.Now:yyyy/MM/dd HH:mm:ss.fff} MessageId:{messageId}, Sent {msg}");
+            });
+
+
+            //await Task.WhenAll(
+            //     Enumerable.Range(1, 100).Select(async item =>
+            //     {
+            //         string msg = $"{item}";
+            //         string messageId = await publisher.PublishAsync(msg);
+            //         Console.WriteLine($"{DateTime.Now:yyyy/MM/dd HH:mm:ss.fff} messageId:{messageId}, Sent {msg}");
+            //     })
+            //    );
 
             Console.ReadKey();
         }
