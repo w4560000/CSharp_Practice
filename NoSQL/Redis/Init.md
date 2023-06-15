26ad6e19d1626bc7e60ad8a1d9b326a599dfd598
w4560000 <39783609+w4560000@users.noreply.github.com>
2023-06-15 16:32:07 +0800

    Redis 備份測試

---
+++ NoSQL/Redis/Init.md
@@ -0,0 +1,17 @@
+﻿
+```
+docker pull redis:7.0
+
+docker run -d --name redis1 -p 6379:6379 redis
+
+docker run -d --name redis1 -p 6379:6379 -v /home/leozheng0411/redis/:/usr/local/etc/redis/ redis
+
+docker exec -it redis1 bash
+```
+
+# Redis 配置
+/usr/local/redis/db/    // RDB、AOF檔
+
+# StackExchange.Redis SDK 配置
+AbortOnConnectFail = 為 true 時，連線到 Redis 發生異常就直接觸發 ConnectionFailed 事件
+					 為 false時，連線到 Redis 發生異常，會立即重試，直到逾時為止
---
+++ NoSQL/Redis/RedisTest/Program.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using StackExchange.Redis;
+
+namespace RedisTest
+{
+    internal class Program
+    {
+        static IConnectionMultiplexer conn = ConnectionMultiplexer.Connect(new ConfigurationOptions()
+        {
+            EndPoints = { "104.199.218.114:6379" },
+            AbortOnConnectFail = false
+        });
+
+        static void Main(string[] args)
+        {
+            var a = Get<string>("a");
+
+            Delete("a");
+            //try
+            //{
+            //    for(int i = 0; i < 100; i++)
+            //    {
+            //        Update("a", i.ToString());
+
+            //        Console.WriteLine($"已更新為 {i}");
+
+            //        Thread.Sleep(1000);
+            //    }
+            //}
+            //catch(Exception ex)
+            //{
+            //    Console.WriteLine(ex.ToString());
+            //}
+
+            Console.ReadKey();
+        }
+
+        private static T? Get<T>(string key)
+        {
+            var value = conn.GetDatabase().StringGet(key);
+
+            if (value.IsNullOrEmpty)
+                return default;
+
+            return JsonConvert.DeserializeObject<T>(value);
+        }
+
+        private static void Update(string key, string data)
+        {
+            conn.GetDatabase().StringSet(key, data);
+        }
+
+
+        private static void Delete(string key)
+        {
+            conn.GetDatabase().KeyDelete(key);
+        }
+    }
+}
