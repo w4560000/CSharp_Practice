861c367e011abce7330b8e15be180ad05bde04dd
w4560000 <39783609+w4560000@users.noreply.github.com>
2025-12-27 00:44:48 +0800

    add - leetcode

---
+++ LeetCode/Problems/Easy/Topic121_Best_Time_To_Buy_And_Sell_Stock.cs
@@ -0,0 +1,43 @@
+﻿using LeetCode.Interface;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Transactions;
+
+namespace LeetCode.Problems.Easy
+{
+    /// <summary>
+    /// Kadane’s Algorithm
+    ///
+    /// 121. Best Time to Buy and Sell Stock
+    /// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+    /// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+    /// Return the maximum profit you can achieve from this transaction.If you cannot achieve any profit, return 0.
+    /// </summary>
+    public class Topic121_Best_Time_To_Buy_And_Sell_Stock : IEntry
+    {
+        public void Main()
+        {
+            //Console.WriteLine(S1(new int[] { 7, 1, 5, 3, 6, 4 }));
+            Console.WriteLine(S1(new int[] { 7,6,4,3,1 }));
+        }
+
+        public int S1(int[] nums)
+        {
+            int buyPrice = nums[0];
+            int profit = 0;
+
+            for (int i = 1; i < nums.Length; i ++)
+            {
+                if (buyPrice > nums[i])
+                    buyPrice = nums[i];
+
+                profit = Math.Max(profit, nums[i] - buyPrice);
+            }
+
+            return profit;
+        }
+    }
+}
---
+++ LeetCode/Problems/Easy/Topic53_Maximum Subarray.cs
@@ -0,0 +1,43 @@
+﻿using LeetCode.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+
+namespace LeetCode.Problems.Easy
+{
+    /// <summary>
+    /// Kadane’s Algorithm
+    ///
+    /// 53. Maximum Subarray
+    /// Given an integer array nums, find the subarray with the largest sum, and return its sum.
+    /// </summary>
+    public class Topic53_Maximum_Subarray : IEntry
+    {
+        public void Main()
+        {
+            Console.WriteLine(S1(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }));
+            //Console.WriteLine(S1(new int[] { 1, 2 }));
+            //Console.WriteLine(S1(new int[] { -2, 1 }));
+        }
+
+        public int S1(int[] nums)
+        {
+            int res = nums[0];
+            int total = 0;
+
+            foreach (int n in nums)
+            {
+                if (total < 0)
+                {
+                    total = 0;
+                }
+
+                total += n;
+                res = Math.Max(res, total);
+            }
+
+            return res;
+        }
+    }
+}
